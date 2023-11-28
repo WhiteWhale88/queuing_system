be7c70b9061235572914bb226c98fbc2f48fe6cc
inesy <inesy@HAIDAROVA>
2023-11-28 11:40:14 +0300

    Добавьте файлы проекта.

---
+++ form.py
@@ -0,0 +1,188 @@
+""" Интерфейс генератора """
+
+from threading import Timer
+import dearpygui.dearpygui as dpg
+import generator as gen
+
+
+QUEUE = gen.Queue()
+DISTRIB = gen.Distributions()
+_SPEED = ["0.5x", "1x", "1.5x", "2x", "5x", "10x"]
+_ISWORK = True
+
+
+def work_generate(type):
+    global _ISWORK
+    match type:
+        case 0:
+            _ISWORK = True
+            dpg.set_value("time_passed", "Время работы: 0")
+            timer(int(dpg.get_value("time_passed").split(" ")[-1]))
+        case 1:
+            _ISWORK = False
+        case 2:
+            _ISWORK = False
+            dpg.configure_item(f"queue_1", items=[])
+            for index in range(1, len(QUEUE.queues)):
+                dpg.delete_item(f"flow_{index + 1}")
+            QUEUE.queues = [[]]
+            QUEUE.size_queue = []
+            QUEUE.waiting_time = []
+            dpg.set_value("avg_size", "Средний размер очереди: 0")
+            dpg.set_value("avg_wait", "Среднее время ожидания в очереди: 0")
+
+
+def timer(sec):
+    """Отсчет времени"""
+    QUEUE.decrease()
+    if sec == DISTRIB.value_ex:
+        QUEUE.put(DISTRIB.value_norm)
+        DISTRIB.gen_next_values()
+
+    sums = sum(sum(q) for q in QUEUE.queues)
+    for index in range(len(QUEUE.queues)):
+        dpg.configure_item(f"queue_{index + 1}", items=QUEUE.queues[index])
+        load = 0
+        if sums != 0:
+            load = round(sum(QUEUE.queues[index]) / sums, 2)
+        dpg.set_value(f"load_{index + 1}", f"Нагруженность: {load}%")
+
+    dpg.set_value("avg_size", f"Средний размер очереди: {QUEUE.get_avg_size()}")
+    dpg.set_value(
+        "avg_wait", f"Среднее время ожидания в очереди: {QUEUE.get_avg_wait()}"
+    )
+    dpg.set_value("time_passed", f"Время работы: {sec}")
+    sec += 1
+    if _ISWORK:
+        Timer(1 / float(dpg.get_value("speed")[:-1]), lambda: timer(sec)).start()
+
+
+def change_count_queue():
+    """Меняет количество очередей"""
+
+    def add_queue():
+        """Добавляет очередь"""
+        QUEUE.add_queue()
+        with dpg.group(parent="queues", tag=f"flow_{len(QUEUE.queues)}"):
+            dpg.add_text(f"Очередь {len(QUEUE.queues)}")
+            dpg.add_text("Нагруженность: 0%", tag=f"load_{len(QUEUE.queues)}")
+            dpg.add_listbox(
+                items=[],
+                width=200,
+                tag=f"queue_{len(QUEUE.queues)}",
+                num_items=15,
+                enabled=True,
+            )
+
+    def del_queue():
+        """Удаляет очередь"""
+        dpg.delete_item(f"flow_{len(QUEUE.queues)}")
+        QUEUE.del_queue()
+
+    if int(dpg.get_value("count_points")) > len(QUEUE.queues):
+        add_queue()
+    else:
+        del_queue()
+
+
+def change_distribution_type():
+    """Меняет тип распределения между очередями"""
+    QUEUE.distrib_type = dpg.get_value("distrib_type")
+
+
+def change_params():
+    """Изменяет параметры распределений"""
+    DISTRIB.mean = float(dpg.get_value("mean"))
+    DISTRIB.sigma = float(dpg.get_value("sigma"))
+    DISTRIB.lambd = float(dpg.get_value("lambd"))
+
+
+if __name__ == "__main__":
+    dpg.create_context()
+
+    with dpg.font_registry():
+        with dpg.font(r"fonts\caviar-dreams.ttf", 20) as font:
+            dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+
+    with dpg.window(tag="Primary Window"):
+        with dpg.group(horizontal=True):
+            with dpg.group():
+                with dpg.group():
+                    dpg.add_text("ПАРАМЕТРЫ ПОТОКА")
+                    dpg.add_text("Интенсивность")
+                    dpg.add_input_text(
+                        default_value=10,
+                        width=300,
+                        decimal=True,
+                        tag="lambd",
+                        callback=change_params,
+                    )
+                    dpg.add_text("Среднее время обработки")
+                    dpg.add_input_text(
+                        default_value=10,
+                        width=300,
+                        decimal=True,
+                        tag="mean",
+                        callback=change_params,
+                    )
+                    dpg.add_text("Среднее время отклонения\nвремени обработки")
+                    dpg.add_input_text(
+                        default_value=3,
+                        width=300,
+                        decimal=True,
+                        tag="sigma",
+                        callback=change_params,
+                    )
+                    dpg.add_text("Количество пунктов обработки")
+                    dpg.add_input_int(
+                        default_value=1,
+                        width=300,
+                        min_value=1,
+                        max_value=5,
+                        tag="count_points",
+                        callback=change_count_queue,
+                    )
+                    dpg.add_text("Скорость отображения")
+                    dpg.add_combo(
+                        default_value=_SPEED[1], items=_SPEED, width=300, tag="speed"
+                    )
+                    dpg.add_text("Распределение нагрузки")
+                    dpg.add_radio_button(
+                        default_value=gen.DISTRIBUTION_TYPES[0],
+                        items=gen.DISTRIBUTION_TYPES,
+                        tag="distrib_type",
+                        callback=change_distribution_type,
+                    )
+
+                with dpg.group():
+                    dpg.add_text("ХАРАКТЕРИСТИКИ ОЧЕРЕДЕЙ")
+                    dpg.add_text("Средний размер очереди: -", tag="avg_size")
+                    dpg.add_text("Среднее время ожидания в очереди: -", tag="avg_wait")
+
+                with dpg.group(horizontal=True):
+                    dpg.add_button(
+                        label="Старт", width=100, callback=lambda: work_generate(0)
+                    )
+                    dpg.add_button(
+                        label="Пауза", width=100, callback=lambda: work_generate(1)
+                    )
+                    dpg.add_button(
+                        label="Стоп", width=100, callback=lambda: work_generate(2)
+                    )
+            with dpg.group():
+                dpg.add_text("ПОТОК")
+                dpg.add_text("Время работы: 0", tag="time_passed")
+                with dpg.group(horizontal=True, tag="queues"):
+                    with dpg.group(parent="queues", tag="flow_1"):
+                        dpg.add_text("Очередь 1")
+                        dpg.add_text("Нагруженность: 0%", tag="load_1")
+                        dpg.add_listbox(
+                            items=[], width=200, tag="queue_1", num_items=15
+                        )
+        dpg.bind_font(font)
+    dpg.create_viewport(title="Generator", width=1200, height=700)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_primary_window("Primary Window", True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
---
+++ generator.py
@@ -0,0 +1,83 @@
+﻿""" Генератор """
+
+import numpy as np
+
+
+DISTRIBUTION_TYPES = ["по размеру очереди", "по нагруженности"]
+
+
+class Distributions:
+    """Распределения"""
+
+    mean = 10
+    sigma = 10
+    lambd = 3
+    value_norm = 0
+    value_ex = 0
+
+    def __init__(self):
+        self.gen_next_values()
+
+    def gen_next_values(self):
+        """Генерирует новую длину интервала и новое значение"""
+
+        def get_number(rand):
+            """Уравнивает число"""
+            num = np.ceil(rand).astype(int)
+            if num <= 0:
+                return 1
+            return num
+
+        self.value_ex += get_number(np.random.exponential(self.lambd))
+        self.value_norm = get_number(np.random.normal(self.mean, self.sigma))
+
+
+class Queue:
+    """Очередь"""
+
+    queues = [[]]
+    distrib_type = DISTRIBUTION_TYPES[0]
+    size_queue = []
+    waiting_time = []
+
+    def __init__(self):
+        pass
+
+    def add_queue(self):
+        """Добавляет очередь в конец"""
+        self.queues.append([])
+
+    def del_queue(self):
+        """Удаляет очередь с конца"""
+        self.queues.pop(0)
+
+    def put(self, element):
+        """Добавляет элемент в конец очереди и его время ожидания"""
+        if self.distrib_type == DISTRIBUTION_TYPES[0]:
+            index = self.queues.index(min(self.queues, key=len))
+        else:
+            index = self.queues.index(min(self.queues, key=sum))
+
+        self.size_queue.append(len(self.queues[index]))
+        self.waiting_time.append(sum(self.queues[index]))
+        self.queues[index].append(element)
+
+    def decrease(self):
+        """Уменьшает очереди"""
+        for item in range(len(self.queues)):
+            if len(self.queues[item]) > 0:
+                self.queues[item][0] -= 1
+                if self.queues[item][0] == 0:
+                    self.queues[item].pop(0)
+
+    def get_avg_size(self):
+        """Возвращает средний размер очереди"""
+        if len(self.size_queue) == 0:
+            return 0.0
+        return round(sum(self.size_queue) / len(self.size_queue), 2)
+
+    def get_avg_wait(self):
+        """Возвращает среднее время ожидания в очереди"""
+        if len(self.waiting_time) == 0:
+            return 0.0
+        return round(sum(self.waiting_time) / len(self.waiting_time), 2)
